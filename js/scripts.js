f4d157b887e63b0decf12ebd14956e2c1a1c5809
postolenko <denis.postolenko@gmail.com>
2021-12-07 22:52:35 +0200

    ALLBC

--- js/scripts.js
+++ js/scripts.js
@@ -644,44 +644,32 @@ $(document).ready(function() {
 
   // ---------
 
-  $(document).on("click", "[data-popup-link]",  function(e) {
-    e.preventDefault();
-    popupName = $(this).attr("data-popup-link");
-    div = document.createElement('div');
-    div.style.overflowY = 'scroll';
-    div.style.width = '50px';
-    div.style.height = '50px';
-    div.style.visibility = 'hidden';
-    document.body.appendChild(div);
-    scrollWidth = div.offsetWidth - div.clientWidth;
-    document.body.removeChild(div);
-    $("body").addClass("fixed");
-    $("body").css({
-        "position" : "fixed",
-        "top" :  -$(document).scrollTop() + "px",
-        "overflow" : "hidden",
-        "right" : 0,
-        "left" : 0,
-        "bottom" : 0,
-        "padding-right" : scrollWidth + "px"
+    $(document).on("click", "[data-popup-link]",  function(e) {
+      e.preventDefault();
+      popupName = $(this).attr("data-popup-link");
+      div = document.createElement('div');
+      div.style.overflowY = 'scroll';
+      div.style.width = '50px';
+      div.style.height = '50px';
+      div.style.visibility = 'hidden';
+      document.body.appendChild(div);
+      scrollWidth = div.offsetWidth - div.clientWidth;
+      document.body.removeChild(div);
+      $("body").addClass("fixed");
+      $("body").css({
+          "position" : "fixed",
+          "top" :  -$(document).scrollTop() + "px",
+          "overflow" : "hidden",
+          "right" : 0,
+          "left" : 0,
+          "bottom" : 0,
+          "padding-right" : scrollWidth + "px"
+      });
+      $(".popup_bg").fadeIn(300);
+      $("[data-popup = '"+ popupName +"']").fadeIn(300);
     });
-    $(".popup_bg").fadeIn(300);
-    $("[data-popup = '"+ popupName +"']").fadeIn(300);
-  });
-  $(document).on("click", ".close_popup, .popup_bg", function(e) {
-    e.preventDefault();
-    curTop = $("body").css("top");
-    curTop = Math.abs(parseInt(curTop, 10));
-    $("body").attr("style", "");
-    if (curTop !== 0) {
-        $("html").scrollTop(curTop);
-    }
-    $("body").removeClass("fixed");
-    $(".popup_bg").fadeOut(300);
-    $("[data-popup]").fadeOut(300);
-  });
-  $(this).keydown(function(eventObject){
-    if (eventObject.which == 27 && $("body").hasClass("fixed")) {
+    $(document).on("click", ".close_popup, .popup_bg", function(e) {
+      e.preventDefault();
       curTop = $("body").css("top");
       curTop = Math.abs(parseInt(curTop, 10));
       $("body").attr("style", "");
@@ -691,26 +679,38 @@ $(document).ready(function() {
       $("body").removeClass("fixed");
       $(".popup_bg").fadeOut(300);
       $("[data-popup]").fadeOut(300);
-    }
-  });
-  $(document).on("mouseup", function(e) {
-    if($(".popup").is(":visible")) {
-      e.preventDefault();
-      hide_element = $(".popup_content");
-      if (!hide_element.is(e.target)
-          && hide_element.has(e.target).length === 0) {
-          curTop = $("body").css("top");
-          curTop = Math.abs(parseInt(curTop, 10));
-          $("body").attr("style", "");
-          if (curTop !== 0) {
-              $("html").scrollTop(curTop);
-          }
-          $("body").removeClass("fixed");
-          $(".popup_bg").fadeOut(300);
-          $("[data-popup]").fadeOut(300);
+    });
+    $(this).keydown(function(eventObject){
+      if (eventObject.which == 27 && $("body").hasClass("fixed")) {
+        curTop = $("body").css("top");
+        curTop = Math.abs(parseInt(curTop, 10));
+        $("body").attr("style", "");
+        if (curTop !== 0) {
+            $("html").scrollTop(curTop);
+        }
+        $("body").removeClass("fixed");
+        $(".popup_bg").fadeOut(300);
+        $("[data-popup]").fadeOut(300);
       }
-    }
-  });
+    });
+    $(document).on("mouseup", function(e) {
+      if($(".popup").is(":visible")) {
+        e.preventDefault();
+        hide_element = $(".popup_content");
+        if (!hide_element.is(e.target)
+            && hide_element.has(e.target).length === 0) {
+            curTop = $("body").css("top");
+            curTop = Math.abs(parseInt(curTop, 10));
+            $("body").attr("style", "");
+            if (curTop !== 0) {
+                $("html").scrollTop(curTop);
+            }
+            $("body").removeClass("fixed");
+            $(".popup_bg").fadeOut(300);
+            $("[data-popup]").fadeOut(300);
+        }
+      }
+    });
 
   var im = new Inputmask("+380 (99)-999-99-99");
   im.mask($("input[type = 'tel']"));
