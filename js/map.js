6e3d539247bf7c6271417d294cac4872d2d3e173
postolenko <denis.postolenko@gmail.com>
2021-12-29 13:52:58 +0200

    ALLBC

--- js/map.js
+++ js/map.js
@@ -190,6 +190,7 @@ function initMap() {
   const map = new google.maps.Map(document.getElementById("object_map"), {
     zoom: 16,
     center: {lat: 50.478209954172236, lng: 30.431968155699604},
+    styles: styles
   });
 
   const flats = [
@@ -218,6 +219,7 @@ function initMap() {
       infoWindow.open(marker.getMap(), marker);
     });
   });
+
 }
 
 function mapCreate(id, latval, lngval, z) {
@@ -238,6 +240,30 @@ function mapCreate(id, latval, lngval, z) {
     });
 }
 
+function mapObject() {
+    const image2 = "img/map_pin.svg";
+    const map3 = new google.maps.Map(document.getElementById("locMap"), {
+        center: {lat: 50.44889, lng: 30.494567},
+        scrollwheel: false,
+        scaleControl: false,
+        zoom: 16,
+        styles: styles
+    });
+    const marker3 = new google.maps.Marker({
+        map: map3,
+        draggable: false,
+        animation: google.maps.Animation.DROP,
+        position: {lat: 50.44889, lng: 30.494567},
+        icon: image2,
+        title: ''
+    });
+}
+
 window.addEventListener("load", function(){
-    initMap();
+    if($("#object_map").length > 0) {
+        initMap();
+    }
+    if($("#locMap").length > 0) {
+        mapObject();
+    }    
 });
