4c5aaa69b2a18a533add3b054a07d0da5b5b9876
postolenko <denis.postolenko@gmail.com>
2022-02-08 10:02:53 +0200

    ALLBC

--- js/scripts.js
+++ js/scripts.js
@@ -1308,50 +1308,10 @@ $(document).ready(function() {
 
   // -------------
 
+ $(document).on("click",".drop_image_card_wrapp .trash", function(e) {
+    e.preventDefault();
+    parent = $(this).closest(".drop_image_card_wrapp");
+    parent.remove();
+ });
+
 });
-
-//   document.addEventListener('DOMContentLoaded', (event) => {
-
-//   function handleDragStart(e) {
-//     this.style.opacity = '0.4';
-//   }
-
-//   function handleDragEnd(e) {
-//     this.style.opacity = '1';
-
-//     items.forEach(function (item) {
-//       item.classList.remove('over');
-//     });
-//   }
-
-//   function handleDragOver(e) {
-//     if (e.preventDefault) {
-//       e.preventDefault();
-//     }
-
-//     return false;
-//   }
-
-//   function handleDragEnter(e) {
-//     this.classList.add('over');
-//   }
-
-//   function handleDragLeave(e) {
-//     this.classList.remove('over');
-//   }
-
-//   function handleDrop(e) {
-//     e.stopPropagation(); // препятствует перенаправлению в браузере.
-//     return false;
-//   }
-
-//   let items = document.querySelectorAll('.drop_image_card_wrapp');
-//   items.forEach(function(item) {
-//     item.addEventListener('dragstart', handleDragStart);
-//     item.addEventListener('dragover', handleDragOver);
-//     item.addEventListener('dragenter', handleDragEnter);
-//     item.addEventListener('dragleave', handleDragLeave);
-//     item.addEventListener('dragend', handleDragEnd);
-//     item.addEventListener('drop', handleDrop);
-//   });
-// });
